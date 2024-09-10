5912993d7a6372084cccc25c8e52e6e06b620991
BballNihal <nihalsaleena@gmail.com>
2024-09-10 16:15:16 -0400

    double threaded kinda kinda kinda 7 second boo yah!

--- HW1/temp.cpp
+++ HW1/temp.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h> // C++ supports this header for bool type
+#include <stdbool.h>
+#include <thread> // Include the thread library
+#include <math.h>
+
+void mark_non_primes(bool primes[], uint64_t start, uint64_t end, uint64_t n) {
+    for (uint64_t i = start; i <= end; i += 2) {
+        if (i * i <= n && primes[i]) {
+            for (uint64_t j = i * i; j <= n; j += 2 * i) {
+                primes[j] = false;
+            }
+        }
+    }
+}
 
 void eratosthenes(bool primes[], uint64_t n) {
     uint64_t count = 0;
@@ -19,15 +31,14 @@ void eratosthenes(bool primes[], uint64_t n) {
         primes[i] = false;
     }
 
-    // Sieve of Eratosthenes starting from 3 and skipping even numbers
-    for (uint64_t i = 3; i * i <= n; i += 2) {
-        if (primes[i]) {
-            // Mark all odd multiples of i as non-prime
-            for (uint64_t j = i * i; j <= n; j += 2 * i) {
-                primes[j] = false;
-            }
-        }
-    }
+    uint64_t sqrt_n = static_cast<uint64_t>(sqrt(n));
+
+    // Use two threads to mark non-prime numbers
+    std::thread t1(mark_non_primes, primes, 3, sqrt_n / 2, n);
+    std::thread t2(mark_non_primes, primes, sqrt_n / 2 + 1, sqrt_n, n);
+
+    t1.join();
+    t2.join();
 
     // Count the number of primes
     for (uint64_t i = 2; i <= n; i++) {
