2c7df6069058955d3877b571f0e6842de4575331
BballNihal <nihalsaleena@gmail.com>
2024-09-09 23:47:55 -0400

    oh yeah

--- HW1/doublethreaded.cpp
+++ HW1/doublethreaded.cpp
@@ -2,6 +2,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+// 0  0  1  0  1  0  1  1  1   0   1   0   1   1   1   0                               1
+// i=97
+//O(log log n)
+
+
 void eratosthenes(bool primes[], uint64_t n) {
     uint64_t count = 0;
 
