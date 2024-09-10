e876e25e8794a42ba798d6efb7d7e89b9cf9f8cd
Thomas <v.u.t.thomas@gmail.com>
2024-09-10 02:07:12 -0400

    yes

--- HW1/4-threaded.cpp
+++ HW1/4-threaded.cpp
@@ -5,33 +5,36 @@
 #include <vector>
 using namespace std;
 
-void eratosthenesMultithread(vector<bool>& primes, uint64_t a ,uint64_t b) {
-
+void eratosthenesMultithread(vector<bool>& primes, uint64_t a ,uint64_t b, uint64_t* pcount) {
+    uint64_t count = 0;
     for (uint64_t i = 2; i <= b; i++) {
         if (primes[i]) {
+            if(i >= a){
+                count++;
+            }
             for (uint64_t j = i * i; j <= b; j += i) {
                 primes[j] = false;
             }
         }
     }
 
-
+    *pcount = count;
 }
 
 int main() {
 
     //initialize variables and array
-    uint64_t n = 100;
+    uint64_t n = 1000000000;
     vector<bool> primes(n+1, false);
     for (uint64_t i = 2; i <= n; i++) {
         primes[i] = true;
     }
-    uint64_t count = 0;
+    uint64_t count = 0,count1 = 0,count2 = 0,count3 = 0,count4 = 0;
 
-    thread t1(eratosthenesMultithread, ref(primes), 2 , n/4);
-    thread t2(eratosthenesMultithread, ref(primes), n/4+1, n/2);
-    thread t3(eratosthenesMultithread, ref(primes), n/2+1, 3*n/4);
-    thread t4(eratosthenesMultithread, ref(primes), 3*n/4+1, n);
+    thread t1(eratosthenesMultithread, ref(primes), 2 , n/4, &count1);
+    thread t2(eratosthenesMultithread, ref(primes), n/4+1, n/2, &count2);
+    thread t3(eratosthenesMultithread, ref(primes), n/2+1, 3*n/4, &count3);
+    thread t4(eratosthenesMultithread, ref(primes), 3*n/4+1, n, &count4);
 
     //join threads
     t1.join();
@@ -40,17 +43,18 @@ int main() {
     t4.join();
 
     // Print the primes
-    for (uint64_t i = 0; i <= n; i++) {
-        if (primes[i]) {
-            printf("%ld ", i);
-        }
-    }
+    // for (uint64_t i = 0; i <= n; i++) {
+    //     if (primes[i]) {
+    //         printf("%ld ", i);
+    //     }
+    // }
 
     //print count
-    for (uint64_t i = 2; i <= n; i++) {
-        if (primes[i]) {
-            count++;
-        }
-    }
-    cout << '\n' << count << '\n';
+    // for (uint64_t i = 2; i <= n; i++) {
+    //     if (primes[i]) {
+    //         count++;
+    //     }
+    // }
+    count = count1 + count2 + count3 + count4;
+    cout << "\n There are " << count << " primes\n";
 }
